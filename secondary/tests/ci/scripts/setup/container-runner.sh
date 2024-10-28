681eb735a5379f3475711aeac6d9c7eeeb307b22
Dhruvil Shah <dsdruvil8@gmail.com>
2024-10-28 19:05:43 +0530

    MB-100 fix broken c script for pause
    Change-Id: I832ccdae3ee58071467788a31e3363561e5225f7
    Signed-off-by: Dhruvil Shah <dsdruvil8@gmail.com>

--- secondary/tests/ci/scripts/setup/container-runner.sh
+++ secondary/tests/ci/scripts/setup/container-runner.sh
@@ -2,7 +2,9 @@
 
 echo "container runner started"
 
-echo '#include <unistd.h>\nint main(){for(;;)pause();}' > pause.c
+echo '#include <unistd.h>
+int main(){for(;;)pause();}
+' > pause.c
 gcc -o pause pause.c
 
 [ -z "$CONTAINER_INIT_SCRIPT" ] && export CONTAINER_INIT_SCRIPT="$ciscripts_dir/secondary/tests/ci/scripts/build"
