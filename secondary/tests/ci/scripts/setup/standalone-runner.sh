c9d8b6d704e6b2ebe4536bb3efb7541ea6ffcd34
Dhruvil Shah <dsdruvil8@gmail.com>
2024-10-29 12:55:43 +0530

    MB-100 standalone echo to command line
    Change-Id: I84426fb2b16a50455a56ea0eac45ebb150a6451d
    Signed-off-by: Dhruvil Shah <dsdruvil8@gmail.com>

--- secondary/tests/ci/scripts/setup/standalone-runner.sh
+++ secondary/tests/ci/scripts/setup/standalone-runner.sh
@@ -8,14 +8,8 @@ source $HOME/.cienv
 
 echo "running standalone runner - $WORKSPACE"
 
-whoami
-
-cd /var/www
-ls -al /var
-
 export TS="$(date +%d.%m.%Y-%H.%M)"
-echo '<html><head></head><body><pre>' >/var/www/gsi-current.html
-chmod a+rx /var/www/gsi-current.html
+echo '<html><head></head><body><pre>'
 
 cd $WORKSPACE
 
@@ -23,19 +17,16 @@ cd $WORKSPACE
 sed -i 's/SET (TAGS "jemalloc")/SET (TAGS "jemalloc 2ici_test")/' $WORKSPACE/goproj/src/github.com/couchbase/indexing/CMakeLists.txt
 
 builder
+test $? -eq 0 || exit 2
 
-dotest 1>>/var/www/gsi-current.html 2>&1
+dotest
 rc=$?
-echo '</pre>' >>/var/www/gsi-current.html
+echo '</pre>'
 
 if [ $rc -eq 0 ]; then status=pass; else status=fail; fi
-echo '<pre>' >>/var/www/gsi-current.html
+echo '<pre>'
 gzip ${WORKSPACE}/logs.tar 2>&1 1>/dev/null
-echo "Version: <a href='versions-$TS.cfg'>versions-$TS.cfg</a>" >>/var/www/gsi-current.html
-echo "Build Log: <a href='make-$TS.log'>make-$TS.log</a>" >>/var/www/gsi-current.html
-echo "Server Log: <a href='logs-$TS.tar.gz'>logs-$TS.tar.gz</a>" >>/var/www/gsi-current.html
-echo "</pre><h1>Finished</h1></body></html>" >>/var/www/gsi-current.html
-cp /var/www/gsi-current.html /var/www/gsi-$TS.$status.html
-mv ${WORKSPACE}/make.log /var/www/make-$TS.log
-mv ${WORKSPACE}/logs.tar.gz /var/www/logs-$TS.tar.gz
-mv ${WORKSPACE}/versions.cfg /var/www/versions-$TS.cfg
+echo "Version: <a href='versions-$TS.cfg'>versions-$TS.cfg</a>"
+echo "Build Log: <a href='make-$TS.log'>make-$TS.log</a>"
+echo "Server Log: <a href='logs-$TS.tar.gz'>logs-$TS.tar.gz</a>"
+echo "</pre><h1>Finished</h1></body></html>"
